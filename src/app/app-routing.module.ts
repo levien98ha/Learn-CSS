72f098dc06e3b916244bddadb584d3271b527ab9
viennl <viennl@vnext.com.vn>
2021-05-10 20:19:17 +0700

    create app

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,16 @@
+import { HomeComponent } from './component/home/home.component';
+import { DefaultLayoutComponent } from './component/DefaultLayout/default-layout/default-layout.component';
+import { HeaderComponent } from './component/DefaultLayout/header/header.component';
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    component: HomeComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,20 @@
+import { HomeComponent } from './component/home/home.component';
+import { FooterComponent } from './component/DefaultLayout/footer/footer.component';
+import { HeaderComponent } from './component/DefaultLayout/header/header.component';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { DefaultLayoutComponent } from './component/DefaultLayout/default-layout/default-layout.component'
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    DefaultLayoutComponent,
+    HeaderComponent,
+    FooterComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
